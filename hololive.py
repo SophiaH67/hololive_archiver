3ab0e080815184d7fd4da4650ba0568ff14a8b3c
marnixah <marnix@hagehoeve.com>
2021-02-24 12:57:18 +0100

    feat: added hololive api module

---
+++ hololive.py
@@ -0,0 +1,27 @@
+import requests
+import json
+import threading
+import time
+
+class streams:
+    schedule = {}
+    sync_thread = None
+
+def sync():
+    res = requests.request("GET", "http://127.0.0.1:5000/schedules")
+    streams.schedule = json.loads(res.text)
+
+def periodic_sync():
+    while True:
+        """Sync every 1 minute, no rate limiting is needed,
+        but it makes it so that python isn't busy with a sync all the time"""
+        sync()
+        time.sleep(60)
+        
+
+streams.sync_thread = threading.Thread(target=periodic_sync)
+streams.sync_thread.run()
+
+if __name__ == "__main__":
+    
+    pass
